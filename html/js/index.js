523385132dc6fb4043e6c7055f63f20c17c66c48
Valdis Aglonietis <va12058@lu.lv>
2017-07-17 04:49:18 +0300

    Deleted html folder and timeline.html inside it. Removed uselss comment from index.js

--- html/js/index.js
+++ html/js/index.js
@@ -27,19 +27,3 @@ function calculatePoints() {
     }
     document.getElementById('totalPoints').innerHTML = totalPoints;
 }
-   /* var array=document.getElementsByName('qty');
-    var totalPoints=0;
-    var multipliers = [2.5,3.5,2.0,2.0];
-    document.getElementById('totalPoints').value=totalPoints;
-    alert(array[i].value+'COOL');
-    for(var i=0;i<4;i++) {
-        if (!/^[0-9]+$/.test(array[i].value)) {
-            document.getElementById('total').value = "Ievadi ciparus :(";
-            return;
-        }
-    }
-    for(var i=0;i<4;i++) {
-        totalPoints +=arr[i].value * multipliers[i];
-    }
-    document.getElementById('totalPoints').innerHTML=totalPoints;
-}*/
